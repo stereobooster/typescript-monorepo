e6e383e9dfcecc0bd042ed30aa60384a07c6afc6
stereobooster <stereobooster@gmail.com>
2019-05-29 19:01:39 +0200

    Add interpackage dependencies

--- packages/b/src/B.tsx
+++ packages/b/src/B.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { d } from "@stereobooster/d";
 
 export type BProps = {};
 
 export const B: React.FC<BProps> = ({ children }) => {
+  d();
   return <div>B: {children}</div>;
 };
 
